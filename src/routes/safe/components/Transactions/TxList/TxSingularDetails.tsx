ce009ba62c339a95e99741283e88f16a4c53d47f
nttnguyen136 <nttnguyen136@gmail.com>
2022-03-15 16:18:07 +0700

    fix router

--- src/routes/safe/components/Transactions/TxList/TxSingularDetails.tsx
+++ src/routes/safe/components/Transactions/TxList/TxSingularDetails.tsx
@@ -16,7 +16,10 @@ import {
   SAFE_ADDRESS_SLUG,
 } from 'src/routes/routes'
 import { Centered } from './styled'
-import { getTransactionWithLocationByAttribute, historyTransactions } from 'src/logic/safe/store/selectors/gatewayTransactions'
+import {
+  getTransactionWithLocationByAttribute,
+  historyTransactions,
+} from 'src/logic/safe/store/selectors/gatewayTransactions'
 import { TxLocationContext } from './TxLocationProvider'
 import { AppReduxState } from 'src/store'
 import { logError, Errors } from 'src/logic/exceptions/CodedException'
@@ -31,6 +34,7 @@ import { currentChainId } from 'src/logic/config/store/selectors'
 import { QueueTxList } from './QueueTxList'
 import { HistoryTxList } from './HistoryTxList'
 import { fetchSafeTransactionById } from 'src/services'
+import { MESSAGES_CODE } from 'src/services/constant/message'
 
 const TxSingularDetails = (): ReactElement => {
   const { [SAFE_ADDRESS_SLUG]: safeTxHash = '' } = useParams<SafeRouteSlugs>()
@@ -78,7 +82,15 @@ const TxSingularDetails = (): ReactElement => {
       let txDetails: any
       try {
         // txDetails = await fetchSafeTransaction(safeTxHash)
-        txDetails = await fetchSafeTransactionById(txId, safeAddress)
+        const res = await fetchSafeTransactionById(txId, safeAddress)
+        const { ErrorCode, Data } = res
+        if (ErrorCode !== MESSAGES_CODE.SUCCESSFUL.ErrorCode) {
+          const txsRoute = generateSafeRoute(SAFE_ROUTES.TRANSACTIONS, extractPrefixedSafeAddress())
+          history.push(txsRoute)
+        } else {
+          txDetails = Data
+        }
+
       } catch (e) {
         logError(Errors._614, e.message)
         return
