935cb96a905640ed7376836c7f2128450f1772d4
nttnguyen136 <nttnguyen136@gmail.com>
2022-05-11 09:16:50 +0700

    add new chain info

--- src/logic/keplr/constants/chainsInfo.ts
+++ src/logic/keplr/constants/chainsInfo.ts
@@ -1,6 +1,6 @@
 import { ChainInfo } from '@keplr-wallet/types'
 
-export const ChainsInfo: { [chainId: string]: ChainInfo } = {
+export const ChainsInfo: { [chainId: string]: any } = {
   ['aura-testnet']: {
     features: ['no-legacy-stdTx'],
     chainId: 'aura-testnet',
@@ -234,4 +234,94 @@ export const ChainsInfo: { [chainId: string]: ChainInfo } = {
       high: 4,
     },
   },
+  ['serenity-testnet-001']: {
+    chainId: "serenity-testnet-001",
+    chainName: "Aura Serenity TestNet",
+    rpc: "https://rpc.serenity.aura.network",
+    rest: "https://lcd.serenity.aura.network",
+    bip44: {
+      coinType: 118,
+    },
+    bech32Config: {
+      bech32PrefixAccAddr: "aura",
+      bech32PrefixAccPub: "aura" + "pub",
+      bech32PrefixValAddr: "aura" + "valoper",
+      bech32PrefixValPub: "aura" + "valoperpub",
+      bech32PrefixConsAddr: "aura" + "valcons",
+      bech32PrefixConsPub: "aura" + "valconspub",
+    },
+    currencies: [{
+      coinDenom: "AURA",
+      coinMinimalDenom: "uaura",
+      coinDecimals: 6,
+      // coinGeckoId: "aura",
+    }, ],
+    feeCurrencies: [{
+      coinDenom: "AURA",
+      coinMinimalDenom: "uaura",
+      coinDecimals: 6,
+      // coinGeckoId: "uaura",
+    }, ],
+    stakeCurrency: {
+      coinDenom: "AURA",
+      coinMinimalDenom: "uaura",
+      coinDecimals: 6,
+      // coinGeckoId: "uaura",
+    },
+    coinType: 118,
+    gasPriceStep: {
+      low: 0.001,
+      average: 0.0025,
+      high: 0.004
+    },
+    features: ['no-legacy-stdTx'],
+    walletUrlForStaking: "https://serenity.aurascan.io/validators",
+    logo: "https://i.imgur.com/zi0mTYb.png",
+    explorer: "https://serenity.aurascan.io/"
+  },
+  ['halo-testnet-001']: {
+    chainId: "halo-testnet-001",
+    chainName: "Aura Halo TestNet",
+    rpc: "https://rpc.halo.aura.network",
+    rest: "https://lcd.halo.aura.network",
+    bip44: {
+      coinType: 118,
+    },
+    bech32Config: {
+      bech32PrefixAccAddr: "aura",
+      bech32PrefixAccPub: "aura" + "pub",
+      bech32PrefixValAddr: "aura" + "valoper",
+      bech32PrefixValPub: "aura" + "valoperpub",
+      bech32PrefixConsAddr: "aura" + "valcons",
+      bech32PrefixConsPub: "aura" + "valconspub",
+    },
+    currencies: [{
+      coinDenom: "AURA",
+      coinMinimalDenom: "uaura",
+      coinDecimals: 6,
+      // coinGeckoId: "aura",
+    }, ],
+    feeCurrencies: [{
+      coinDenom: "AURA",
+      coinMinimalDenom: "uaura",
+      coinDecimals: 6,
+      // coinGeckoId: "uaura",
+    }, ],
+    stakeCurrency: {
+      coinDenom: "AURA",
+      coinMinimalDenom: "uaura",
+      coinDecimals: 6,
+      // coinGeckoId: "uaura",
+    },
+    coinType: 118,
+    gasPriceStep: {
+      low: 0.001,
+      average: 0.0025,
+      high: 0.004
+    },
+    features: ['no-legacy-stdTx'],
+    walletUrlForStaking: "https://halo.aurascan.io/validators",
+    logo: "https://i.imgur.com/zi0mTYb.png",
+    explorer: "https://halo.aurascan.io/"
+  }
 }
