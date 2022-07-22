99e8bbb59adfecefb41147ff6b1d9aab42d140a7
halong99t <phamhande@gmail.com>
2022-07-23 02:05:42 +0700

    remove libraly

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles'
 import { SnackbarProvider } from 'notistack'
 import { useDispatch, useSelector } from 'react-redux'
 import styled from 'styled-components'
+
 import AlertIcon from 'src/assets/icons/alert.svg'
 import CheckIcon from 'src/assets/icons/check.svg'
 import ErrorIcon from 'src/assets/icons/error.svg'
