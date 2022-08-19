176d54e7b7a3619272d80a5b06f5d15ba3c2757e
phamha <phamhande@gmail.com>
2022-08-19 14:35:08 +0700

    update box card

---
+++ src/components/CardVoting/index.tsx
@@ -0,0 +1,15 @@
+import { ReactElement } from 'react'
+import BoxCard from '../BoxCard'
+import { Breadcrumb, BreadcrumbElement, Menu, Text } from '@aura/safe-react-components'
+
+function CardVoting(props): ReactElement {
+  return (
+    <BoxCard>
+      <Text size="lg" color="disableAura">
+        Hallo
+      </Text>
+    </BoxCard>
+  )
+}
+
+export default CardVoting
--- src/routes/safe/components/Voting/index.tsx
+++ src/routes/safe/components/Voting/index.tsx
@@ -1,6 +1,32 @@
 import React, { ReactElement } from 'react'
+import { Breadcrumb, BreadcrumbElement, Menu, Text } from '@aura/safe-react-components'
+import Col from 'src/components/layout/Col'
+import CardVoting from 'src/components/CardVoting'
+import Block from 'src/components/layout/Block'
+
 function Voting(props): ReactElement {
-  return <>Voting</>
+  return (
+    <>
+      <Menu>
+        <Col start="sm" sm={12} xs={12}>
+          <Breadcrumb>
+            <BreadcrumbElement
+              color="white"
+              iconType="votingAura"
+              text="Voting"
+              // counter={addressBook?.length.toString()}
+            />
+          </Breadcrumb>
+        </Col>
+      </Menu>
+      <Block>
+        {' '}
+        <Col start="sm" sm={6} xs={12}>
+          <CardVoting />
+        </Col>
+      </Block>
+    </>
+  )
 }
 
 export default Voting
