bb609a27db29b295908e77db3da1458a64a81407
phamha <phamhande@gmail.com>
2022-08-19 15:11:56 +0700

    check out serenity

--- src/components/CardVoting/index.tsx
+++ src/components/CardVoting/index.tsx
@@ -1,13 +1,35 @@
 import { ReactElement } from 'react'
 import BoxCard from '../BoxCard'
 import { Breadcrumb, BreadcrumbElement, Menu, Text } from '@aura/safe-react-components'
+import Col from 'src/components/layout/Col'
+import styled from 'styled-components'
+import StatusCard from '../StatusCard'
+import Row from '../layout/Row'
+const TitleNumberStyled = styled.div`
+  font-weight: 510;
+  font-size: 20px;
+  line-height: 26px;
+  color: #b4b8c0;
+`
+
+const TitleStyled = styled.div`
+  font-weight: 510;
+  font-size: 20px;
+  line-height: 26px;
+  color: rgba(255, 255, 255, 1);
+`
 
 function CardVoting(props): ReactElement {
   return (
     <BoxCard>
-      <Text size="lg" color="disableAura">
-        Hallo
-      </Text>
+      <Col start="sm" layout="column" sm={6} xs={12}>
+        <TitleNumberStyled>#60</TitleNumberStyled>
+        <TitleStyled>Increase MaxValidator value</TitleStyled>
+      </Col>
+      <Col end="sm" sm={6} xs={12}>
+        <StatusCard />
+      </Col>
+      <Row>hello</Row>
     </BoxCard>
   )
 }
