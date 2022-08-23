8744df110b57690bc1a613e6db24435ff43d66c7
Minh Bui <72347448+clanV2@users.noreply.github.com>
2022-08-23 15:13:03 +0700

    add staking first card

---
+++ src/components/CardStaking/style.ts
@@ -0,0 +1,17 @@
+import { createStyles } from '@material-ui/core'
+
+export const styles = createStyles({
+  stakingOverview: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    gap: '8px',
+  },
+  stakingOverviewTextContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space - between',
+    alignItems: 'center',
+    gap: '8px',
+  },
+})
