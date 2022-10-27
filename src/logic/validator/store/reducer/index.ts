42010494619535628d35e4afb3b1b835319563d5
imhson <phamhoangson99@gmail.com>
2022-10-27 13:52:04 +0700

    done

---
+++ src/logic/validator/store/reducer/index.ts
@@ -0,0 +1,32 @@
+import { Action, handleActions } from 'redux-actions'
+import { FETCH_ALL_VALIDATORS } from './../actions/index'
+
+export const VALIDATOR_REDUCER_ID = 'validator'
+
+export type ValidatorType = {
+  name: string
+  operatorAddress: string
+  picture?: string
+  votingPower: { number: string; percentage: string }
+}
+
+export type ValidatorStateType = {
+  allValidators: ValidatorType[]
+}
+
+export const initialState = {
+  allValidators: [],
+}
+
+type ValidatorPayloadType = ValidatorStateType | ValidatorType[]
+
+const validatorReducer = handleActions<ValidatorStateType, ValidatorPayloadType>(
+  {
+    [FETCH_ALL_VALIDATORS]: (state, action: Action<ValidatorType[]>) => ({
+      allValidators: action.payload,
+    }),
+  },
+  initialState,
+)
+
+export default validatorReducer
