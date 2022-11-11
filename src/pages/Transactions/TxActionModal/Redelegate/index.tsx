d254b9acaed3011108705f8062ce51027877cded
imhson <phamhoangson99@gmail.com>
2022-11-11 16:31:53 +0700

    fix gas type

--- src/pages/Transactions/TxActionModal/Redelegate/index.tsx
+++ src/pages/Transactions/TxActionModal/Redelegate/index.tsx
@@ -46,7 +46,7 @@ export default function Execute({ open, onClose, data, sendTx, rejectTx, disable
     const denom = getCoinMinimalDenom()
     const sendFee = {
       amount: coins(data?.txDetails?.fee, denom),
-      gas: data?.txDetails?.gas,
+      gas: data?.txDetails?.gas.toString(),
     }
 
     const Data: any = {
