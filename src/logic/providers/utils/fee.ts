992d1e78b4d967670cd2f9d06ee4598e38160901
imhson <phamhoangson99@gmail.com>
2022-12-06 17:36:34 +0700

    fix fee cal

--- src/logic/providers/utils/fee.ts
+++ src/logic/providers/utils/fee.ts
@@ -3,7 +3,7 @@ import BigNumber from 'bignumber.js'
 const calculateGasFee = (gas: number, gasPrice: number, decimal: number): number => {
   return +new BigNumber(+gas)
     .times(new BigNumber(+gasPrice))
-    .div(new BigNumber(10).pow(6))
+    .div(new BigNumber(10).pow(decimal))
     .toFixed(6)
 }
 
